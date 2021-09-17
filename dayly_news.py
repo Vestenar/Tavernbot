879c8075130728ba4b16fc53b33a6a1dadfc7f95
vestenar <vestenar@gmail.com>
2021-09-17 23:32:35 +0200

    TOKEN save place changed to settings

--- dayly_news.py
+++ dayly_news.py
@@ -1,17 +1,12 @@
 import requests
 import getgodville
-import json
 import getinfo
-from settings import CHATS, MY_ID
-
-with open('params.txt') as init_file:
-    bot_params = json.loads(init_file.read())
-    bot_token = bot_params["bot_token"]
+from settings import CHATS, MY_ID, BOT_TOKEN
 
 prognoz = 'Всем доброе утро! Утренняя сводка новостей от бармена:\n' + getgodville.god_prognoz() + '\n'
 for chat_id in CHATS:
     leaving = getgodville.god_guild(CHATS[chat_id])
-    url = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={prognoz + leaving}'
+    url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={prognoz + leaving}'
     resp = requests.get(url)
 
 chat_id = MY_ID
@@ -20,5 +15,5 @@ text = getinfo.get_promo()
 if text:
     text = 'Свежие коды для genshin: \n' + text
 else:
-    url = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={text}'
+    url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={text}'
     requests.get(url)
