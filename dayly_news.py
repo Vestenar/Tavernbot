e35c557bb96bfac9b036ced542706a55616ee404
vestenar <vestenar@gmail.com>
2021-10-02 13:38:36 +0200

    genshin removed

--- dayly_news.py
+++ dayly_news.py
@@ -1,7 +1,6 @@
 import requests
 import getgodville
-import getinfo
-from settings import CHATS, MY_ID, BOT_TOKEN
+from settings import CHATS, BOT_TOKEN
 
 prognoz = 'Всем доброе утро! Утренняя сводка новостей от бармена:\n' + getgodville.god_prognoz() + '\n'
 for chat_id in CHATS:
@@ -9,11 +8,3 @@ for chat_id in CHATS:
     url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={prognoz + leaving}'
     resp = requests.get(url)
 
-chat_id = MY_ID
-text = getinfo.get_promo()
-
-if text:
-    text = 'Свежие коды для genshin: \n' + text
-else:
-    url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={text}'
-    requests.get(url)
