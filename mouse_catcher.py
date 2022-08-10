788f55571d24862a698f93ffee51de9ad6d1df19
vestenar <vestenar@gmail.com>
2022-08-10 18:05:04 +0200

    new user meet rat bugfix

--- mouse_catcher.py
+++ mouse_catcher.py
@@ -11,11 +11,14 @@ bot_token = settings.BOT_TOKEN
 bot = TeleBot(bot_token)
 my_id = settings.MY_ID
 chats = settings.CHATS
-# chats = [settings.MY_ID]
-now = pytz.timezone('UTC')
 delay_min = 20 * 60
 delay_max = 60 * 60
 ratio = 7
+# chats = [settings.MY_ID]
+# now = pytz.timezone('UTC')
+# delay_min = 5
+# delay_max = 6
+# ratio = 1
 
 
 def score_counter(chat_id, user_id, score):
@@ -33,16 +36,22 @@ def score_counter(chat_id, user_id, score):
 def save_user(user_id, username):
     with open('users.json', 'r') as file:
         user_list = json.loads(file.read())
-        if str(user_id) not in user_list:
-            user_list[str(user_id)] = username
-            with open('users.json', 'w') as file:
-                file.write(json.dumps(user_list))
+    if str(user_id) not in user_list:
+        user_list[str(user_id)] = username
+        with open('users.json', 'w') as file:
+            file.write(json.dumps(user_list))
 
 
 def get_score(chat_id, user_id):
     with open('mouse_scores.json') as file:
-        scores = json.loads(file.read())["mice_caught"][str(chat_id)]
-    return scores[str(user_id)]
+        scores_list = json.loads(file.read())["mice_caught"]
+        if str(chat_id) not in scores_list:
+            score_counter(chat_id, user_id, 0)
+            return 0
+        if str(user_id) in scores_list[str(chat_id)]:
+            return scores_list[str(chat_id)][str(user_id)]
+        else:
+            return 0
 
 
 def show_scores(chat_id):
@@ -64,8 +73,10 @@ def show_scores(chat_id):
 if __name__ == '__main__':
     while True:
         time.sleep(randint(delay_min, delay_max))
-        utc_time = now.localize(datetime.utcnow())
-        if 7 <= ((utc_time.hour + 27) % 24) < 23:
+        # utc_time = now.localize(datetime.utcnow())
+        msk_zone = pytz.timezone('Europe/Moscow')
+        now = datetime.now(tz=msk_zone)
+        if 7 <= now.hour < 23:
             for chat in chats:
                 rnd_mouse = choice(['mouse'] * ratio + ['rat'] * (10 - ratio))
                 mouse_appear(bot, chat, rnd_mouse)
@@ -73,3 +84,5 @@ if __name__ == '__main__':
 
     # print(show_scores(-1001320841683))
     # score_counter(settings.MY_ID, settings.MY_ID, 2)
+    # print(get_score(my_id, my_id))
+    # save_user(123, 'name')
