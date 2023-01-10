ac5b8990f5526ecdc279bd4d9d2ded2e699cbf2d
vestenar <vestenar@gmail.com>
2023-01-10 13:53:09 +0200

    date added

--- dayly_news.py
+++ dayly_news.py
@@ -3,15 +3,18 @@ import getgodville
 from settings import CHATS, BOT_TOKEN, ZST_ID
 from jump_counter import autostart_timers
 from telebot import TeleBot
+from datetime import datetime
 
 
-prognoz = 'Всем доброе утро! Утренняя сводка новостей от бармена:\n' + getgodville.god_prognoz() + '\n'
+now_str = datetime.now().strftime('%m.%d.%Y года')
+prognoz = f'Всем доброе утро! Сегодня {now_str}\nУтренняя сводка новостей от бармена:\n {getgodville.god_prognoz()}\n'
 coupon = getgodville.get_coupon()
 for chat_id in CHATS:
     leaving = getgodville.god_guild(CHATS[chat_id])
 
     url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={prognoz + leaving + coupon}'
     resp = requests.get(url)
+    print(f'{now_str}: {CHATS[chat_id]} status {resp.status_code}')
     # print(prognoz + leaving + coupon)
 
 # ------<<<------ Перезапуск таймеров ------>>>------
