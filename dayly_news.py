d9bf82466f2851f96f0640a3633f99011acedc9b
vestenar <vestenar@gmail.com>
2025-03-30 18:18:50 +0300

    spaceline added, logging off

--- dayly_news.py
+++ dayly_news.py
@@ -7,14 +7,14 @@ from datetime import datetime
 
 
 now_str = datetime.now().strftime('%d.%m.%Y года')
-prognoz = f'Всем доброе утро! Сегодня {now_str}\nУтренняя сводка новостей от бармена:\n {getgodville.god_prognoz()}\n'
+prognoz = f'Всем доброе утро! Сегодня {now_str}\nУтренняя сводка новостей от бармена:\n{getgodville.god_prognoz()}\n\n'
 coupon = getgodville.get_coupon()
 for chat_id in CHATS:
     leaving = getgodville.god_guild(CHATS[chat_id])
 
     url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={prognoz + leaving + coupon}'
     resp = requests.get(url)
-    print(f'{now_str}: {CHATS[chat_id]} status {resp.status_code}')
+    # print(f'{now_str}: {CHATS[chat_id]} status {resp.status_code}')
     # print(prognoz + leaving + coupon)
 
 # ------<<<------ Перезапуск таймеров ------>>>------
